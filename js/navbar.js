ef9783217e05972ed68b40621772237ae768f44c
Andi Balser <andreas.balser@stud-mail.uni-wuerzburg.de>
2021-10-31 15:34:59 +0100

    improvements

--- js/navbar.js
+++ js/navbar.js
@@ -4,5 +4,5 @@ function toggleBurgerMenu() {
     const navbar = document.getElementById("mobile");
 
     burgerList.style.opacity = burgerList.style.opacity === "0" ? "1" : "0";
-    navbar.style.height = navbar.style.height === "60px" ? "140px" : "60px";
+    navbar.style.height = navbar.style.height === "60px" ? "180px" : "60px";
 }
